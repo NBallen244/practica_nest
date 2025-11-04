e082df6ac4ca81cd402e5a34e3c192ec787e1adb
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-11-04 18:10:12 -0500

    avance

--- src/museum/museum.service.spec.ts
+++ src/museum/museum.service.spec.ts
@@ -6,10 +6,27 @@ import { Repository } from 'typeorm';
 import { MuseumEntity } from './museum.entity';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { MuseumService } from './museum.service';
+import { faker } from '@faker-js/faker';
+
 
 describe('MuseumService', () => {
  let service: MuseumService;
  let repository: Repository<MuseumEntity>;
+ let museumsList: MuseumEntity[] = [];
+
+ const seedDatabase = async () => {
+   repository.clear();
+   museumsList = [];
+   for(let i = 0; i < 5; i++){
+       const museum: MuseumEntity = await repository.save({
+       name: faker.company.name(),
+       description: faker.lorem.sentence(),
+       address: faker.location.secondaryAddress(),
+       city: faker.location.city(),
+       image: faker.image.url()})
+       museumsList.push(museum);
+   }
+ }
 
  beforeEach(async () => {
    const module: TestingModule = await Test.createTestingModule({
@@ -19,10 +36,17 @@ describe('MuseumService', () => {
 
    service = module.get<MuseumService>(MuseumService);
    repository = module.get<Repository<MuseumEntity>>(getRepositoryToken(MuseumEntity));
+  await seedDatabase();
  });
   
  it('should be defined', () => {
    expect(service).toBeDefined();
+ });
+
+ it('findAll should return all museums', async () => {
+   const museums: MuseumEntity[] = await service.findAll();
+   expect(museums).not.toBeNull();
+   expect(museums).toHaveLength(museumsList.length);
  });
 
 });
