e40c8d758291112b2e8aef44489d02e57509a32a
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2025-11-09 12:21:21 -0500

    pruebas unitarias internas de museum y museum-artwork

---
+++ src/museum-artwork/museum-artwork.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { MuseumArtworkService } from './museum-artwork.service';
+import { MuseumEntity } from 'src/museum/museum.entity';
+import { ArtworkEntity } from 'src/artwork/artwork.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports:[TypeOrmModule.forFeature([MuseumEntity, ArtworkEntity])],
+  providers: [MuseumArtworkService],
+})
+export class MuseumArtworkModule {}
--- src/museum/museum.service.spec.ts
+++ src/museum/museum.service.spec.ts
@@ -49,4 +49,79 @@ describe('MuseumService', () => {
    expect(museums).toHaveLength(museumsList.length);
  });
 
+ it('findOne should return a museum by id', async () => {
+   const storedMuseum: MuseumEntity = museumsList[0];
+   const museum: MuseumEntity = await service.findOne(storedMuseum.id);
+   expect(museum).not.toBeNull();
+   expect(museum.name).toEqual(storedMuseum.name)
+   expect(museum.description).toEqual(storedMuseum.description)
+   expect(museum.address).toEqual(storedMuseum.address)
+   expect(museum.city).toEqual(storedMuseum.city)
+   expect(museum.image).toEqual(storedMuseum.image)
+ });
+
+ it('findOne should throw an exception for an invalid museum', async () => {
+   await expect(() => service.findOne("0")).rejects.toHaveProperty("message", "The museum with the given id was not found")
+ });
+
+ it('create should return a new museum', async () => {
+   const museum: MuseumEntity = {
+     id: "",
+     name: faker.company.name(),
+     description: faker.lorem.sentence(),
+     address: faker.location.secondaryAddress(),
+     city: faker.location.city(),
+     image: faker.image.url(),
+     exhibitions: [],
+     artworks: []
+   }
+
+   const newMuseum: MuseumEntity = await service.create(museum);
+   expect(newMuseum).not.toBeNull();
+
+   const storedMuseum: MuseumEntity|null = await repository.findOne({where: {id: newMuseum.id}})
+   expect(storedMuseum).not.toBeNull();
+   expect(storedMuseum!.name).toEqual(newMuseum.name)
+   expect(storedMuseum!.description).toEqual(newMuseum.description)
+   expect(storedMuseum!.address).toEqual(newMuseum.address)
+   expect(storedMuseum!.city).toEqual(newMuseum.city)
+   expect(storedMuseum!.image).toEqual(newMuseum.image)
+ });
+ it('update should modify a museum', async () => {
+   const museum: MuseumEntity = museumsList[0];
+   museum.name = "New name";
+   museum.address = "New address";
+    const updatedMuseum: MuseumEntity = await service.update(museum.id, museum);
+   expect(updatedMuseum).not.toBeNull();
+    const storedMuseum: MuseumEntity|null = await repository.findOne({ where: { id: museum.id } })
+   expect(storedMuseum).not.toBeNull();
+   expect(storedMuseum!.name).toEqual(museum.name)
+   expect(storedMuseum!.address).toEqual(museum.address)
+ });
+
+ it('update should throw an exception for an invalid museum', async () => {
+   let museum: MuseumEntity = museumsList[0];
+   museum = {
+     ...museum, name: "New name", address: "New address"
+   }
+   await expect(() => service.update("0", museum)).rejects.toHaveProperty("message", "The museum with the given id was not found")
+ });
+
+ it('delete should remove a museum', async () => {
+   const museum: MuseumEntity = museumsList[0];
+   await service.delete(museum.id);
+    const deletedMuseum: MuseumEntity|null = await repository.findOne({ where: { id: museum.id } })
+   expect(deletedMuseum).toBeNull();
+ });
+
+ it('delete should remove a museum', async () => {
+   const museum: MuseumEntity = museumsList[0];
+   await service.delete(museum.id);
+    const deletedMuseum: MuseumEntity|null = await repository.findOne({ where: { id: museum.id } })
+   expect(deletedMuseum).toBeNull();
+ });
+
+ it('delete should throw an exception for an invalid museum', async () => {
+   await expect(() => service.delete("0")).rejects.toHaveProperty("message", "The museum with the given id was not found")
+ });
 });
